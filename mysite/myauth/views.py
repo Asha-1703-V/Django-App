6100bd8ca77cc860a603d18b61248c55e6ba4e78
Asha_ <valein.danil@mail.ru>
2025-02-25 22:23:06 +0800

    9.6 - приложение myauth установлено и настроено

--- mysite/myauth/views.py
+++ mysite/myauth/views.py
@@ -1,9 +1,12 @@
-from django.http import HttpRequest
+from django.contrib.auth.views import LogoutView
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse, reverse_lazy
+from django.views import View
 
 
-def login_view(request: HttpRequest):
+def login_view(request: HttpRequest) -> HttpResponse:
     if request.method == "GET":
         if request.user.is_authenticated:
             return redirect('/admin/')
@@ -19,3 +22,32 @@ def login_view(request: HttpRequest):
         return  redirect("/admin/")
 
     return render(request, "myauth/login.html", {"error": "Invalid login credentails"})
+
+def logout_view(request: HttpRequest):
+    logout(request)
+    return redirect(reverse("myauth:login"))
+
+class MyLogoutView(LogoutView):
+    next_page = reverse_lazy("myauth:login")
+
+class MyLogoutPage(View):
+    def get(self, request):
+        logout(request)
+        return redirect('myauth:login')
+
+def set_cookie_view(request: HttpRequest) -> HttpResponse:
+    response = HttpResponse('Cookie set')
+    response.set_cookie("fizz", "buzz", max_age=3600)
+    return response
+
+def get_cookie_view(request: HttpRequest) -> HttpResponse:
+    value = request.COOKIES.get("fizz", "default value")
+    return HttpResponse(f"Cookie value: {value!r}")
+
+def set_session_view(request: HttpRequest) -> HttpResponse:
+    request.session["foobar"] = "spameggs"
+    return HttpResponse("Session set!")
+
+def get_session_view(request: HttpRequest) -> HttpResponse:
+    value = request.session.get("foobar", "default")
+    return HttpResponse(f"Session value: {value!r}")
